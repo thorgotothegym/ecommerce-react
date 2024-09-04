1df6139df9f41fda067f5aaece0c07a13538afbf
Aleix Vidal <al3ix.vidal@gmail.com>
2024-09-04 20:14:40 +0200

    fix: breadcrumb, layout and metadata

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,10 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next'
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "./globals.css";
+import { User } from "@/components/ui/User";
+import { Footer } from "@/components/ui/Footer";
+import { Breadcrumb } from "@/components/ui/Breadcrumb/Breadcrumb";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -26,7 +29,21 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        {children}
+      <section
+      aria-label='shipping'
+      className='w-full container flex justify-center m-1'
+    >
+      free shipping
+    </section>
+    <section aria-label='menu' className='w-full py-4 bg-muted'>
+      <div className='container flex flex-row-reverse px-4 md:px-6'>
+        <User email='john@example.com' fullName='John Doe' />
+      </div>
+    </section>
+    <section aria-label='breadcrumb' className="container flex flex-row px-4 md:px-6">
+      <Breadcrumb />
+      </section>{children}
+    <Footer />
       </body>
     </html>
   );
