11b14e2ca716a4956bc2beb29c3891c0ffd2e69b
Aleix Vidal <al3ix.vidal@gmail.com>
2024-09-04 19:54:32 +0200

    feat: simple version breadcrumb

---
+++ frontend/src/components/ui/Breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+type Props = {
+  paths?: string[]
+}
+
+export const Breadcrumb: React.FC<Props> = () => {
+  const paths = usePathname()
+  const pathNames = paths.split('/').filter(path => path)
+  const capitalizeLinks: boolean = true
+  return (
+    <ul>
+      <li>
+        {pathNames.map((link, index) => {
+          let href = `/${pathNames.slice(0, index + 1).join('/')}`
+          let itemLink = capitalizeLinks
+            ? link[0].toUpperCase() + link.slice(1, link.length)
+            : link
+          return (
+            <Link href={href} className='pl-1 pr-1' key={link}>
+              {itemLink}
+            </Link>
+          )
+        })}
+      </li>
+    </ul>
+  )
+}
