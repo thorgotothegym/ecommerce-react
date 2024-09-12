b914bd84efa1850a111ce2b1e98537c00f4bf11e
Aleix Vidal <al3ix.vidal@gmail.com>
2024-09-12 11:22:24 +0200

    feat: add different type to useContext

--- frontend/src/context/UserContext.tsx
+++ frontend/src/context/UserContext.tsx
@@ -1,9 +1,14 @@
 'use client'
 import React, { createContext, useState, FC, ReactNode } from 'react'
 
+type User = {
+  name?: string
+  email: string
+}
+
 type MyContextType = {
-  value: string
-  setValue: (newValue: string) => void
+  value: User
+  setValue: React.Dispatch<React.SetStateAction<User>>
 }
 
 const MyContext = createContext<MyContextType | undefined>(undefined)
@@ -13,7 +18,7 @@ type MyContextProviderProps = {
 }
 
 export const MyContextProvider: FC<MyContextProviderProps> = ({ children }) => {
-  const [value, setValue] = useState<string>('')
+  const [value, setValue] = useState<User>({ name: '', email: '' })
 
   return (
     <MyContext.Provider value={{ value, setValue }}>
