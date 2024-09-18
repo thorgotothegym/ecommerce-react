a3c8261a5b5bbb2ef99df9ef240508a1faf105a4
Aleix Vidal <al3ix.vidal@gmail.com>
2024-09-18 14:36:58 +0200

    feat: import usersRouter to main

--- backend/src/app/index.ts
+++ backend/src/app/index.ts
@@ -1,14 +1,13 @@
 import express, { json } from "express";
 import { PORT } from "./config";
+import usersRouter from "./routes/users";
 
 const app = express();
 
 app.use(json());
 app.disable("x-powered-by");
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "all good" });
-});
+app.use("/", usersRouter);
 
 app.listen(PORT, () => {
   console.log(`server listening on port http://localhost:${PORT}`);
